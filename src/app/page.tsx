b23e6fcd7461f1fc23e382f72ba32e54dbb42bd4
areeba <areebayaqoob2421@gmail.com>
2025-09-19 23:51:52 -0700

    king

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 export default function HomePage() {
   return (
     <div className="text-center">
-      <h1 className="text-3xl font-bold mb-4">Welcome to Home Page "We create modern, fast and responsive websites..."</h1>
-      <img src="/nic.jpg" alt="Home" className="mx-auto rounded-lg shadow-md w-80" />
+      <h1 className="text-3xl font-extrabold mb-4">Welcome to Home Page "We create modern, fast and responsive websites..."</h1>
+      <img src="/via.jpg" alt="Home" className="mx-auto rounded-lx shadow-md w-80" />
     </div>
   );
 }
